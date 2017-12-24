c04c6f395850ad4106009dc5ade2c47ef1a6d47a
NYAMUJOSEPH <jnjogu@cytonn.com>
2017-12-24 19:16:25 +0300

    Laravel Intergration

--- resources/assets/js/components/Auth/Forms.vue
+++ resources/assets/js/components/Auth/Forms.vue
@@ -26,7 +26,7 @@
 
             <!-- Register -->
             <div class="reveal" id="register" data-reveal>
-                <h5><i class="fi-plus"></i>&nbsp;Register</h5>
+                <h5><i class="fi-plus"></i>&nbsp;Register New Account</h5>
                 <hr/>
 
                 <form>
