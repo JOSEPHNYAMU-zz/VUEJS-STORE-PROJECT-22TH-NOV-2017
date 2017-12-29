72050fded4b5f0d7f9331f1fa716acb8b6378b25
NYAMUJOSEPH <jnjogu@cytonn.com>
2017-12-29 19:11:39 +0300

    This commit fixes the banner image to be full screen

--- resources/assets/js/components/Banner.vue
+++ resources/assets/js/components/Banner.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="small-12 columns" id="pad">
         <div class="grid-x">
-            <img class="pictures" src="../images/back.png">
+            <img class="pictures" style="width: 100% !important;" src="../images/back.png">
         </div>
     </div>
 </template>
