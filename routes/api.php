06d05581b7f30c48c4ca9d0134c48aa4538276e5
NYAMUJOSEPH <jnjogu@cytonn.com>
2018-01-17 14:14:24 +0300

    Cors setup at Kernel

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,5 @@
 <?php
-Route::group(['middleware' => 'cors'], function () {
 Route::post('/register', 'AuthController@register');
 Route::post('/login', 'AuthController@login');
 Route::post('/logout', 'AuthController@logout');
 Route::resource('items', 'ItemsController');
-});
